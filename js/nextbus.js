55687211e927d70e09f15de1e111551b37a00b87
Jacob Ulrich <julrich@gmail.com>
2017-07-10 15:28:49 -0700

    Favorites partially implemented

--- js/nextbus.js
+++ js/nextbus.js
@@ -3,6 +3,8 @@ const UPDATE_INTERVAL = 2000;
 
 let map;
 let markers = [];
+let stops = [];
+let favorites = [];
 let lastTime = 0;
 
 function initRoute() {
@@ -18,9 +20,9 @@ function initRoute() {
       let routeLines = new google.maps.Polyline({
         path: routeCoords,
         geodesic: true,
-        strokeColor: '#FF0000',
-        strokeOpacity: 0.5,
-        strokeWeight: 5
+        strokeColor: "black",
+        strokeOpacity: 0.7,
+        strokeWeight: 3
       });
 
       routeLines.setMap(map);
@@ -28,11 +30,72 @@ function initRoute() {
     }
 
     initStops(data.route.stop);
+  }).fail(function(data) {
+    console.error("There was an error retrieving data from the API.");
   });
 }
 
-function initStops(stops) {
-  console.log(stops);
+function initStops(stopData) {
+  let iterator = 0;
+
+  for (const stop of stopData) {
+    stops[iterator] = new google.maps.Marker(
+      {
+        map: map,
+        label: "",
+        optimized: false,
+        zIndex: 1,
+        icon: {
+          path: google.maps.SymbolPath.CIRCLE,
+          // url: "img/stop_marker_scaled.png",
+          // scaledSize: new google.maps.Size(20, 20),
+          // origin: new google.maps.Point(0,0),
+          // anchor: new google.maps.Point(10, 16),
+          scale: 4,
+          // rotation: 0,
+          // fillColor: "red"
+          strokeColor: "blue",
+          fillColor: "blue",
+          fillOpacity: 1
+        }
+      });
+
+    const coords = {lat: Number(stop.lat), lng: Number(stop.lon)};
+
+    stops[iterator].setPosition(coords);
+
+    stops[iterator].set("stopId", stop.stopId);
+    stops[iterator].set("title", stop.title);
+    stops[iterator].set("infoWindow", new google.maps.InfoWindow({
+      content: stop.title
+    }));
+
+    stops[iterator].addListener('click', function() {
+      getArrivalTime(this);
+      closeAllInfoWindows();
+      this.infoWindow.open(map, this);
+    });
+
+    iterator++;
+  }
+}
+
+function getArrivalTime(stop) {
+  $.ajax({ url: `http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=seattle-sc&r=FHS&s=${stop.stopId}` }).done(function(data) {
+
+    let contentString = "<ul>";
+
+    contentString += `<li>${stop.title}</li>`;
+
+    for (const arrivalTime of data.predictions.direction.prediction) {
+      contentString += `<li>${arrivalTime.minutes}</li>`;
+    }
+
+    contentString += "</ul>";
+    stop.infoWindow.setContent(contentString);
+  }).fail(function(data) {
+    console.error("There was an error retrieving data from the API.");
+  });
 }
 
 function getStreetCarDataInitial() {
@@ -48,10 +111,13 @@ function getStreetCarDataInitial() {
           easing: "jswing",
           speedMph: convertKmHrToMph(vehicle.speedKmHr),
           markerLastTime: vehicle.secsSinceReport,
+          zIndex: 10,
+          optimized: false,
           icon: {
-            path: google.maps.SymbolPath.FORWARD_OPEN_ARROW,
-            scale: 4,
-            rotation: 0
+            path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+            scale: 5,
+            rotation: 0,
+            fillOpacity: 1
           }
         });
 
@@ -60,12 +126,14 @@ function getStreetCarDataInitial() {
       const fillColor = getIconColor(iterator);
 
       markers[iterator].icon.strokeColor = fillColor;
+      markers[iterator].icon.fillColor = fillColor;
 
       markers[iterator].set("infoWindow", new google.maps.InfoWindow({
         content: ""
       }));
 
       markers[iterator].addListener('click', function() {
+        closeAllInfoWindows();
         this.infoWindow.open(map, this);
       });
 
@@ -79,6 +147,8 @@ function getStreetCarDataInitial() {
 
     setInterval(getStreetCarData, UPDATE_INTERVAL);
     setInterval(updateIntervals, 1000);
+  }).fail(function(data) {
+    console.error("There was an error retrieving data from the API.");
   });
 }
 
@@ -86,7 +156,6 @@ function getStreetCarData() {
   $.ajax({ url: `http://webservices.nextbus.com/service/publicJSONFeed?command=vehicleLocations&a=seattle-sc&r=FHS&t=${lastTime}` }).done(function(data) {
 
     if (!data.vehicle) {
-      console.log("No updates", data);
       return;
     }
 
@@ -114,6 +183,8 @@ function getStreetCarData() {
         console.log(marker.icon.strokeColor, vehicle.heading, marker.icon.rotation);
       }
     }
+  }).fail(function(data) {
+    console.error("There was an error retrieving data from the API.");
   });
 }
 
@@ -171,19 +242,25 @@ function initMap() {
   const centerRoute = {lat: 47.609809, lng: -122.320826};
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
-    center: centerRoute
+    center: centerRoute,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    clickableIcons: false
+  });
+
+  map.addListener("click", function() {
+    closeAllInfoWindows();
   });
 }
 
 function getIconColor(color) {
-  let fillColor = "purple";
+  let fillColor = "black";
 
   switch(color) {
     case 0:
       fillColor = 'orange';
       break;
     case 1:
-      fillColor = "blue";
+      fillColor = "purple";
       break;
     case 2:
       fillColor = "red";
@@ -194,6 +271,41 @@ function getIconColor(color) {
   }
 
   return fillColor;
+}
+
+function closeAllInfoWindows() {
+  for (const stop of stops) {
+    stop.infoWindow.close();
+  }
+
+  for (const marker of markers) {
+    marker.infoWindow.close();
+  }
+}
+
+function addFavorite(stopId) {
+  favorites.push(stopId);
+
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+}
+
+function getFavorites() {
+  favorites = JSON.parse(localStorage.getItem("favorites"));
+
+  const $collapsible = $(".collapsible");
+
+  for (const favorite of favorites) {
+    const $collapseLi = $("<li>");
+    const $stopDiv = $("<div>").addClass("collapsible-header").text(favorite);
+    const $stopInfo = $("<div>").addClass("collapsible-body").text("Arriving in 0 minutes");
+    const $stopIcon = $("<i>").addClass("material-icons").text("favorite");
+
+    $stopDiv.append($stopIcon);
+    $collapseLi.append($stopDiv, $stopInfo);
+    $collapsible.append($collapseLi);
+  }
+
+  $('.collapsible').collapsible();
 }
 
 $(".button-collapse").sideNav();
